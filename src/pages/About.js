5116ec457711416f3e30f5d14158fad5af5ca5d4
TamlynSpringer <tamlynspringer@gmail.com>
2023-01-18 20:13:00 +0100

    Changed about bg color

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,17 +8,17 @@ const About = ({ children }) => {
     <>
     <Nav children={children} />
     <main className='px-20 py-10'>
-        <article className='px-16 py-10'>
+        <article className='px-20 py-10 bg-neutral-300'>
           <h3 lassName='mb-10  text-l'>
             Created by Tamlyn Springer.
           </h3>
-          <h3 className='mb-10'>During the School of Applied Technology's JavaScript Fall 2022 career program. <a href='https://www.salt.dev/' target='_blank' rel='noreferrer'>{parse(saltLogo)}</a></h3>
+          <h3 className='mb-10'>During the School of Applied Technology's <br></br>JavaScript Fall 2022 career program. <a href='https://www.salt.dev/' target='_blank' rel='noreferrer'>{parse(saltLogo)}</a></h3>
         
           <h3>Connect:</h3>
           <span><a href='https://www.linkedin.com/in/tamlyn-springer-v/' target='_blank' rel='noreferrer'><i className='fa-brands fa-linkedin fa-2x linkedin-logo cursor-pointer'></i></a> </span>
             <span><a href='https://github.com/TamlynSpringer' target='_blank' rel='noreferrer'><i className='fa-brands fa-square-github fa-2x github-logo cursor-pointer'></i></a></span>
         </article>
-        <article className='px-16 py-10'>
+        <article className='px-20 py-10 bg-neutral-300'>
           <h3>
             Tech stack:
           </h3>
